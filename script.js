9325e2e95312616e96a8cda24111a84bb5406f31
Ansh Kushwaha <ansh.kushwaha26@gmail.com>
2024-01-17 13:13:20 +0530

    Added analog clock

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-function getTime(){
-    var today = new Date();
+function getTime() {
+    let today = new Date();
 
-    var hours = today.getHours();
-    var minutes = today.getMinutes();
-    var seconds = today.getSeconds();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+
+    // Analog updation
+    let secDeg = seconds * 6;
+    let minDeg = minutes * 6;
+    let houDeg = hours * 30 + (minutes / 2);
+    document.getElementById("hour-hand").style.transform = 'rotate(' + houDeg + 'deg)';
+    document.getElementById("minute-hand").style.transform = 'rotate(' + minDeg + 'deg)';
+    document.getElementById("second-hand").style.transform = 'rotate(' + secDeg + 'deg)';
+
 
     let period = "AM";
     //Set (AM/PM)
-    if(hours>=12){
+    if (hours >= 12){
         period = "PM";
     }
     //The colon & div
@@ -18,8 +27,8 @@ function getTime(){
 
     //Adding 0 to time<10
     hours = hours < 10 ? "0"+hours : hours;
-    minutes = minutes < 10 ? "0"+minutes : minutes;
-    seconds = seconds < 10 ? "0"+seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
     document.querySelector(".hours").innerHTML = hours;
     document.querySelector(".minutes").innerHTML = minutes;
@@ -27,8 +36,8 @@ function getTime(){
     document.querySelector(".period").innerHTML = period;
 }
 
-function getDate(){
-    var today = new Date();
+function getDate() {
+    let today = new Date();
 
     const date = today.getDate();
     const month = today.toLocaleString("default", {month: "long"});
@@ -41,44 +50,45 @@ function getDate(){
     document.querySelector(".day").innerHTML = day;
 }
 
-function StyleChange(){
-    var today = new Date();
+function StyleChange() {
+    let today = new Date();
     hour = today.getHours();
-    if(hour>=00 && hour<04){
+    if (hour >= 0 && hour < 04){
         document.body.style.backgroundImage = "url('img/24To4.png')";
-    }
-    else if(hour>=04 && hour<06){
+    } else if (hour >= 04 && hour < 06) {
         document.body.style.backgroundImage = "url('img/4To6.png')";
-    }
-    else if(hour>=06 && hour<08){
+    } else if (hour >= 6 && hour < 8) {
         document.body.style.backgroundImage = "url('img/6To8.png')";
-    }
-    else if(hour>=08 && hour<10){
+    } else if (hour >= 8 && hour < 10) {
         document.body.style.backgroundImage = "url('img/8To10.png')";
-    }
-    else if(hour>=10 && hour<12){
+    } else if (hour >= 10 && hour < 12) {
         document.body.style.backgroundImage = "url('img/10To12.png')";
-    }
-    else if(hour>=12 && hour<14){
+    } else if (hour >= 12 && hour < 14) {
         document.body.style.backgroundImage = "url('img/12To14.png')";
-    }
-    else if(hour>=14 && hour<16){
+    } else if (hour >= 14 && hour < 16) {
         document.body.style.backgroundImage = "url('img/14To16.png')";
-    }
-    else if(hour>=16 && hour<18){
+    } else if (hour >= 16 && hour < 18) {
         document.body.style.backgroundImage = "url('img/16To18.png')";
-    }
-    else if(hour>=18 && hour<20){
+    } else if (hour >= 18 && hour < 20) {
         document.body.style.backgroundImage = "url('img/18To20.png')";
-    }
-    else if(hour>=20 && hour<22){
+    } else if (hour >= 20 && hour < 22) {
         document.body.style.backgroundImage = "url('img/20To22.png')";
-    }
-    else if(hour>=22 && hour<24){
+    } else if (hour >= 22 && hour < 24) {
         document.body.style.backgroundImage = "url('img/22To24.png')";
     }
 }
 
-var updateTime = setInterval(getTime, 1000);
-var updateDate = setInterval(getDate, 1000);
-var updateStyle = setInterval(StyleChange, 1000);
+let updateTime = setInterval(getTime, 1000);
+let updateDate = setInterval(getDate, 1000);
+let updateStyle = setInterval(StyleChange, 1000);
+
+// Digital <-> Analog (2024)
+function changeToAna() {
+    document.getElementById('digital').style.display = "none";
+    document.getElementById('analog').style.display = "flex";
+}
+
+function changeToDig() {
+    document.getElementById('analog').style.display = "none";
+    document.getElementById('digital').style.display = "block";
+}
